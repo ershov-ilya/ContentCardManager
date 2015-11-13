d44c7b983dee9d25592468da82187bbdaa732b9c
ILYA ERSHOV <ershov.ilya@gmail.com>
2015-11-13 13:57:40 +0300

    test data

--- app/jsx/app.jsx
+++ app/jsx/app.jsx
@@ -1,7 +1,66 @@
-console.log('JSX here');
-
 var App = React.createClass({
-  render: function() {
-    return <div>Hello world!</div>;
-  }
+//    $this:null,
+    componentWillMount: function(){
+//        $this=this;
+    },
+    statics: {
+        get: function() {
+            return $this.state.userInput;
+        },
+        set: function(data) {
+            $this.setState({userInput: data});
+        }
+    },
+
+    getInitialState: function() {
+        return {
+            userInput: '',
+            items: []
+        };
+    },
+
+    componentDidMount: function() {
+        $.get(this.props.source, function(response) {
+            if (this.isMounted()) {
+                this.setState({
+                    items: response
+                });
+            }
+        }.bind(this));
+    },
+
+    LoadMore: function(){
+        $.get('json/second.json', function(response) {
+            if (this.isMounted()) {
+                this.setState({
+                    items: $.merge(this.state.items,response)
+                });
+            }
+        }.bind(this));
+    },
+
+    render: function() {
+        return (
+            <div>
+                <ul id="List" className={"row marketing"}>
+                    {
+                        this.state.items.map(function(item, i) {
+                            return <div className={"col-lg-4"} key={i}>
+                                          <h2>{item.title}</h2>
+                                          <p>{item.description}</p>
+                                          <p>{item.img}</p>
+                                          <p><a className={"btn btn-primary"} href="#" role="button">View details »</a></p>
+                                      </div>
+                        })
+                    }
+                </ul>
+      <div className={"jumbotron"}>
+        <p><a className={"btn btn-lg btn-success"} href="#" onClick={this.LoadMore}>Load More!</a></p>
+      </div>
+             </div>
+        );
+    }
 });
+
+// Mounting
+if(typeof App != 'undefined') ReactDOM.render(<App source="json/first.json" />, document.getElementById('App'));
--- app/scripts/jsx/app.js
+++ app/scripts/jsx/app.js
@@ -1,15 +1,102 @@
 'use strict';
 
-console.log('JSX here');
-
 var App = React.createClass({
-  displayName: 'App',
+    displayName: 'App',
 
-  render: function render() {
-    return React.createElement(
-      'div',
-      null,
-      'Hello world!'
-    );
-  }
+    //    $this:null,
+    componentWillMount: function componentWillMount() {
+        //        $this=this;
+    },
+    statics: {
+        get: function get() {
+            return $this.state.userInput;
+        },
+        set: function set(data) {
+            $this.setState({ userInput: data });
+        }
+    },
+
+    getInitialState: function getInitialState() {
+        return {
+            userInput: '',
+            items: []
+        };
+    },
+
+    componentDidMount: function componentDidMount() {
+        $.get(this.props.source, (function (response) {
+            if (this.isMounted()) {
+                this.setState({
+                    items: response
+                });
+            }
+        }).bind(this));
+    },
+
+    LoadMore: function LoadMore() {
+        $.get('json/second.json', (function (response) {
+            if (this.isMounted()) {
+                this.setState({
+                    items: $.merge(this.state.items, response)
+                });
+            }
+        }).bind(this));
+    },
+
+    render: function render() {
+        return React.createElement(
+            'div',
+            null,
+            React.createElement(
+                'ul',
+                { id: 'List', className: "row marketing" },
+                this.state.items.map(function (item, i) {
+                    return React.createElement(
+                        'div',
+                        { className: "col-lg-4", key: i },
+                        React.createElement(
+                            'h2',
+                            null,
+                            item.title
+                        ),
+                        React.createElement(
+                            'p',
+                            null,
+                            item.description
+                        ),
+                        React.createElement(
+                            'p',
+                            null,
+                            item.img
+                        ),
+                        React.createElement(
+                            'p',
+                            null,
+                            React.createElement(
+                                'a',
+                                { className: "btn btn-primary", href: '#', role: 'button' },
+                                'View details »'
+                            )
+                        )
+                    );
+                })
+            ),
+            React.createElement(
+                'div',
+                { className: "jumbotron" },
+                React.createElement(
+                    'p',
+                    null,
+                    React.createElement(
+                        'a',
+                        { className: "btn btn-lg btn-success", href: '#', onClick: this.LoadMore },
+                        'Load More!'
+                    )
+                )
+            )
+        );
+    }
 });
+
+// Mounting
+if (typeof App != 'undefined') ReactDOM.render(React.createElement(App, { source: 'json/first.json' }), document.getElementById('App'));
